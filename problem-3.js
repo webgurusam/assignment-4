ab8f59948b3b44f32e1dbada94cd5e1e90e66064
webgurusam <shyamghosh666@gmail.com>
2023-12-30 14:23:57 +0600

    problem-3 is checking

--- problem-3.js
+++ problem-3.js
@@ -6,22 +6,22 @@ function sortMaker(arr){
         return 'Please, provide us only two elements in number';
     }
     else{
-        const positiveNumbers = [];
+        const positiveNumbers = arr;
         for(let n of arr){
             if(typeof n !== 'number'){
                 return 'Please, input numbers in an array';
             }
             else{
                 if(n > 0){
-                    if(n > positiveNumbers){
-                        positiveNumbers.unshift(n);
-                    }
+                    arr.sort(function(a,b){
+                        return b - a;
+                    });
                 }
             }
         }
         return positiveNumbers;
     }
 }
-const arr = [2,3];
+const arr = [2,4];
 const result = sortMaker(arr);
 console.log(result);
