2a3ed5bbf69ac3de0fb3e25361786008c82f87c4
webgurusam <shyamghosh666@gmail.com>
2023-12-30 12:09:34 +0600

    problem-1 is solved

--- assignment.js
+++ assignment.js
@@ -1,6 +1,15 @@
 function cubeNumber(number){
-
+    if(typeof number !== 'number'){
+        return 'Please, provide a number';
+    }
+    else if(number <= 0){
+        return 'Please, provide a positive number';
+    }
+    else{
+        const cube = number * number * number;
+        return cube;
+    }
 }
-
+const number = 4;
 const result = cubeNumber(number);
 console.log(result);
