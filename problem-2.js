1c03c0c605ea4061bcab890c9d2860296daf32fc
webgurusam <shyamghosh666@gmail.com>
2023-12-30 12:26:25 +0600

    problem-2 is checking

--- problem-2.js
+++ problem-2.js
@@ -2,8 +2,16 @@ function matchFinder(string1, string2){
     if(typeof string1 !== 'string' || typeof string2 !== 'string'){
         return 'Please, provide us string type data';
     }
+    else{
+        if(string1.toLowerCase().includes(string2.toLowerCase())){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
-const string1 = 5;
+const string1 = 'John Doe';
 const string2 = 'ohn';
 const result = matchFinder(string1, string2);
 console.log(result);
